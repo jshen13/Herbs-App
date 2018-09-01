09500a39000f53297bfd5c0b5b5c9b5408963504
Jeffrey Shen <jeffreyshen13@gmail.com>
2018-08-31 23:00:51 -0700

    Initial commit

---
+++ Assets/ButtonPressed.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonPressed : MonoBehaviour {
+    public Button button;
+    public Toggle buttonToggle;
+    public Color selectedColor;
+    public Color notSelectedColor;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    //when the button is clicked
+    public void onClick()
+    {
+        ColorBlock cb = button.colors;
+
+        if (buttonToggle.isOn)
+        {
+            buttonToggle.isOn = false;
+            //cb.normalColor = notSelectedColor;
+            //cb.highlightedColor = notSelectedColor;
+            //button.image.color = notSelectedColor;
+            //button.colors = cb;
+        }
+        else 
+        {
+            buttonToggle.isOn = true;
+            //cb.normalColor = selectedColor;
+            //cb.highlightedColor = selectedColor;
+            //button.colors = cb;
+        }
+    }
+
+    
+}
---
+++ Assets/SelectedHerbs.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class SelectedHerbs : MonoBehaviour {
+    public Toggle[] checks;
+    private ArrayList names;
+    public Text displayHerbsName;
+    public Text displayHerbsPlace;
+    private string[] allHerbs = {"herb 1", "herb 2", "herb 3", "herb 4",
+                                 "herb 5", "herb 6", "herb 7", "herb 8"};
+    private string[] location = {"1, 1", "1, 2", "1, 3", "1, 4",
+                                 "2, 1", "2, 2", "2, 3", "2, 4" };
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+       
+	}
+
+    public void display()
+    {
+
+        
+
+        names = new ArrayList();
+        foreach (Toggle c in checks)
+        {
+            if (c.isOn)
+            {
+                names.Add(c.name);
+            }
+
+
+        }
+        string herbName = "";
+        string herbPlace = "";
+        Debug.Log(names.ToString());
+
+        int index;
+        foreach (string n in names){
+            int.TryParse(n, out index);
+            Debug.Log(index);
+            herbName += allHerbs[index] + "\n";
+            herbPlace += location[index] + "\n";
+            Debug.Log(location[index]);
+        }
+        displayHerbsName.text = herbName;
+        displayHerbsPlace.text = herbPlace;
+    }
+}
